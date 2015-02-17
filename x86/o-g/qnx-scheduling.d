0a94da24d4c025153e2a82997f9ef80b34e1dbeb
Steven Kroh <sk.kroh@gmail.com>
2015-02-17 15:53:52 -0500

    Adding spin stubs, and filled in more of the architecture

---
+++ x86/o-g/qnx-scheduling.d
@@ -0,0 +1,17 @@
+qnx-scheduling.o: \
+ /home/skroh/Documents/rit/classes/2014/spring/performance/proj/qnx-scheduling/qnx-scheduling.c \
+ /opt/qnx650/target/qnx6/usr/include/stdlib.h \
+ /opt/qnx650/target/qnx6/usr/include/sys/platform.h \
+ /opt/qnx650/target/qnx6/usr/include/sys/srcversion.h \
+ /opt/qnx650/target/qnx6/usr/include/sys/compiler_gnu.h \
+ /opt/qnx650/target/qnx6/usr/include/sys/cdefs.h \
+ /opt/qnx650/target/qnx6/usr/include/x86/platform.h \
+ /opt/qnx650/target/qnx6/usr/include/_pack64.h \
+ /opt/qnx650/target/qnx6/usr/include/sys/target_nto.h \
+ /opt/qnx650/target/qnx6/usr/include/_packpop.h \
+ /opt/qnx650/target/qnx6/usr/include/malloc.h \
+ /opt/qnx650/target/qnx6/usr/include/alloca.h \
+ /opt/qnx650/target/qnx6/usr/include/sys/wait.h \
+ /opt/qnx650/target/qnx6/usr/include/signal.h \
+ /opt/qnx650/target/qnx6/usr/include/sys/siginfo.h \
+ /opt/qnx650/target/qnx6/usr/include/stdio.h
