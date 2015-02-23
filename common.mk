35ef16bf825cebdffcbda15911c6e44df03f74da
Stephen Brewster <sxb7781@rit.edu>
2015-02-23 14:09:59 -0500

    SCT 2

--- common.mk
+++ common.mk
@@ -16,7 +16,8 @@ LIBS+=c socket
 #===== EXTRA_SRCVPATH - a space-separated list of directories to search for source files.
 EXTRA_SRCVPATH+=$(PROJECT_ROOT)/fixt $(PROJECT_ROOT)/spin  \
 	$(PROJECT_ROOT)/fixt/impl/rma  \
-	$(PROJECT_ROOT)/fixt/impl/edf $(PROJECT_ROOT)/log
+	$(PROJECT_ROOT)/fixt/impl/edf  \
+	$(PROJECT_ROOT)/fixt/impl/sct $(PROJECT_ROOT)/log
 
 include $(MKFILES_ROOT)/qmacros.mk
 ifndef QNX_INTERNAL
